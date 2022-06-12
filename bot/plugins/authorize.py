3a6e8ea55a07a0554676381e6b93cfb0a02643e0
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-06-12 17:22:25 +0530

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -14,7 +14,7 @@ from bot.helpers.utils import CustomFilters
 
 
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
-REDIRECT_URI = "http://localhost:8080/"
+REDIRECT_URI = "http://127.0.0.1/google_account/authentication"
 
 flow = None
 
