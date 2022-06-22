be4c14c98405f74439caab15bea4dac01e0ab3c4
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-06-22 11:58:52 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         api_id=APP_ID,
         api_hash=API_HASH,
         plugins=plugins,
-        parse_mode="markdown",
+        parse_mode="Markdown2",
         workdir=DOWNLOAD_DIRECTORY
     )
     LOGGER.info('Starting Bot !')
